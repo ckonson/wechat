22f90f3013152ce9149fb866253fd9c8c51395d6
kontem <kontem@kontemdeMacBook-Pro.local>
2016-02-18 17:14:20 +0800

    add cache model

--- src/Cache/FileCache.php
+++ src/Cache/FileCache.php
@@ -3,19 +3,57 @@
  * Created by PhpStorm.
  * User: kontem
  * Date: 16/2/18
- * Time: 15:41
+ * Time: 15:41.
  */
 
 namespace CkWechat\Cache;
 
-
 class FileCache
 {
-    protected $cache_path='';
-    protected $cache_type='';
-    protected $cache_time='';
+    protected $cache_path = '';
+    protected $cache_time = '';
     public function __construct(array $params = null)
     {
-      # code...
+        $this->cache_path = $params['cache_path'];
+        $this->cache_time = $params['cache_time'];
+        $this->init();
+    }
+    public function init()
+    {
+        if (!empty($this->cache_path) && !is_dir($this->cache_path)) {
+            mkdir($this->cache_path, 0755);
+        } elseif (empty($this->cache_path)) {
+            $this->cache_path = '_wechat_cache';
+            mkdir($this->cache_path, 0755);
+        }
+    }
+    public function getKey($key = '')
+    {
+        $result = '';
+        if (empty($key)) {
+            #TODO
+        }
+        $cache_file = $this->cache_path.$key;
+        if (!empty($key) && file_exists($cache_file)) {
+            $result = file_get_contents($cache_file);
+        } else {
+            #TODO
+        }
+    }
+    public function setkey($key = '', $value = '')
+    {
+        if (empty($key) || empty($value)) {
+           #TODO
+        }
+        $cache_file = $this->cache_path.$key
+        if (!is_writable(dirname($cache_file))) {
+            #TODO
+        }else{
+            $len = file_put_contents($cache_file, $value);
+            if (strlen($value) == $len) {
+              return true;
+            }
+            return false;
+        }
     }
 }
--- src/application.php
+++ src/application.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: kontem
  * Date: 16/2/18
- * Time: 15:51
+ * Time: 15:51.
  */
 
 namespace CkWechat;
@@ -13,8 +13,20 @@ use CkWechat\Cache\FileCache;
 
 class Application
 {
-  public function getToken($appId, $secret)
-  {
-    return (new AccessToken($appId, $secret))->getToken();
-  }
+    public function getToken($appId, $secret)
+    {
+        $result = '';
+        $AccessToken_object = new AccessToken($appId, $secret);
+        $cache_key = md5($appId.$secret);
+        $cache_obj = new FileCache();
+        $cache_token = $cache_obj->getKey($cache_key);
+        if (!$cache_token) {
+            $result = $AccessToken_object->getToken();
+            $cache_obj->setkey($cache_key,$result);
+        }else{
+            $result = $cache_token;
+        }
+
+        return $result;
+    }
 }
